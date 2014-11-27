038b449159cdbfd8026b1f325c61f49623b75587
Gabe Schrecker <gabe@octavia.smith.man.ac.uk>
2014-11-27 18:50:48 +0000

    Completed first working version, documented backend, added plus icon

--- backend.py
+++ backend.py
@@ -5,58 +5,92 @@ import getpass
 import os
 import subprocess
        
-def main():
+def open_config():
+    """Locates and opens the sbgrid config file, if none is 
+    found it opens a default file with only the header"""
+    
     conf_path='/home/'+getpass.getuser()+'/.sbgrid.conf'
     if not os.path.isfile(conf_path):
         conf_path='default_sbgrid.conf'
     
     config_file=open(conf_path, 'r')
-    prog_dict, prog_list = find_sbgrid_progs()
-    read_config(config_file)
-    #get_override_name("eman2")
-
-
+    return config_file
 
 def read_config(config_file):
-    config_header = []; h=0
-    config_array = []; a=0
+    """Creates 2 arrays, 1 to store the header lines and
+    another array of list to store any existing overides.
+    New overrides can be added to this array later."""
+    config_header = []
+    config_array = []
     
     for line in config_file:
         s=str(line)
-        if s[0] == "#":
+        #look for #, these indicate header lines, also means comments will be moved above overides
+        if s[0] == "#": 
             config_header.append(s)
-            h=h+1
         else:
+        #uses = to deliniate ProgName from VerNum
             eq_pos = s.find("=")
             config_array.append([s[0:eq_pos],s[eq_pos+1:-1]])
-            a=a+1
+    config_file.close()
 
 #    if len(config_array) == 0:
 #        config_array.append(["None", "None"])
     return config_header, config_array
 
+def write_config(config_header, config_array):
+    conf_path='/home/'+getpass.getuser()+'/.sbgrid.conf'
+    if os.path.isfile(conf_path+'.bak'): os.remove(conf_path+'.bak')    
+    os.rename(conf_path, conf_path+'.bak')
+    config_file=open(conf_path, 'w')    
+    config_file.writelines(config_header)
+    for i in config_array: config_file.write(i[0]+"="+i[1]+"\n")
+    config_file.close()
+        
+    
+
 def find_sbgrid_progs():
-    prog_folder = "/programs/x86_64-linux/"
+    """Searches host file system and lists all installed sbgrid software,
+    makes an alphabetical list for easy browsing and a dict with
+    prog names as keys leading to a list of versions"""
+
+    prog_folder = "/programs/x86_64-linux/" #here thar be binaries
     prog_list = list_top_level_dir(prog_folder)
     prog_list.sort()
     prog_dict={}
-    for app in prog_list:
-        ver_list = list_top_level_dir(prog_folder+app)
+    for prog in prog_list:
+        ver_list = list_top_level_dir(prog_folder+prog)
         ver_list.sort()
-        ver_list.append("disable")
-        prog_dict[app]= ver_list
+        ver_list.append("disable") #not actually a version but it allows for disabling a package
+        prog_dict[prog]= ver_list
     return prog_dict, prog_list
     
-def list_top_level_dir(thedir):            
+def list_top_level_dir(thedir):
+    """just ls -d in python form, dumps output to list"""
     dirlist=[ name for name in os.listdir(thedir) if os.path.isdir(os.path.join(thedir, name)) ]
     return dirlist
 
 def get_override_name(prog_name):
+    """call out to the sbgrid command that gives the override name for packages,
+    although it is normally just all upper case with _X on the end"""
     arg = "-l "+prog_name
     p = subprocess.Popen(['sbgrid', arg], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     out, err = p.communicate()
     ov_nam=out[out.find('Overrides use this shell variable:')+36:]
-    print ov_nam.strip()
+    return ov_nam.strip()
+
+def add_override(config_array, prog, ver):
+    """checks if a version override already exists for this program, if so it
+    edits the old one, if not it adds a new one."""
+    already_added = False    
+    for i in config_array:
+        if i[0] == get_override_name(prog):
+            i[1] = ver
+            already_added = True
+    if already_added == False:
+        config_array.append([get_override_name(prog),ver])
+    return config_array
+        
     
 
 
--- guitest.py
+++ guitest.py
@@ -7,7 +7,7 @@ from PyQt4 import QtGui, QtCore
 
 
 
-class Example(QtGui.QWidget):
+class Example(QtGui.QMainWindow):
     
     def __init__(self):
         super(Example, self).__init__()
@@ -16,51 +16,120 @@ class Example(QtGui.QWidget):
         
     def initUI(self):      
 
-        self.lbl1 = QtGui.QLabel("Select an SBGrid Program", self)
+        lbl1 = QtGui.QLabel("Select an SBGrid Program", self)
+        lbl1.adjustSize() 
+        lbl1.resize
         lbl2 = QtGui.QLabel("Program", self)
         lbl3 = QtGui.QLabel("Version", self)
-
-        prog_sel_combo = QtGui.QComboBox(self)
-        prog_sel_combo.completer()
+        self.lbl4 = QtGui.QLabel("Saved Overrides", self)
+        self.lbl4.adjustSize()
+        self.statusBar().showMessage('Ready')
+   
+        config_file=backend.open_config()
+        self.config_header, self.config_array = backend.read_config(config_file)
         self.prog_dict, prog_list = backend.find_sbgrid_progs()
+
+        self.prog_sel_combo = QtGui.QComboBox(self)
+        self.prog_sel_combo.completer()
         for prog in prog_list:	
-            prog_sel_combo.addItem(prog)
+            self.prog_sel_combo.addItem(prog)
 
         self.ver_sel_combo = QtGui.QComboBox(self)
         self.ver_sel_combo.completer()
 
-        self.prog_btn = QtGui.QPushButton('Run ' +prog_list[0], self)
-        self.prog_btn.setToolTip('This will <b>start</b> the program selected in the dropdown')
-        self.prog_btn.resize(self.prog_btn.sizeHint())
+        self.add_btn = QtGui.QPushButton(self)
+        self.add_btn.setIcon(QtGui.QIcon("icon-plus-512.png"))
+        self.add_btn.setEnabled(False)
+        self.add_btn.setToolTip('Sets the selected <b>version</b> as default for this <b>program</b>. ')
+
+        self.save_btn = QtGui.QPushButton('Save', self)
+        self.save_btn.setEnabled(False)
+        self.save_btn.setToolTip('Save all changes to your config file.')
+        self.save_btn.resize(self.save_btn.sizeHint())
+
+        self.table = QtGui.QTableWidget(self,)
+        self.table.setColumnCount(3)
+        self.table.setEditTriggers(QtGui.QTableWidget.NoEditTriggers)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.populate_table(self.config_array)
         
 	#widget locations
-        self.lbl1.move(25, 25)
+        lbl1.move(25, 25)
        	lbl2.move(25, 50)
         lbl3.move(165, 50)
-        prog_sel_combo.move(25, 75)
-        prog_sel_combo.resize(120, 20)
+        self.prog_sel_combo.move(25, 75)
+        self.prog_sel_combo.resize(120, 20)
         self.ver_sel_combo.move(165, 75)
         self.ver_sel_combo.resize(120, 20)
-        self.prog_btn.move(25,100)
+        self.add_btn.move(300,55)
+        self.add_btn.resize(40,40)
+        self.lbl4.move(25, 110)
+        self.table.move(25,140)
+        self.table.resize(315,150)
+        self.save_btn.move(25,300)
 
 
-        prog_sel_combo.activated[str].connect(self.progSelected)        
-        self.ver_sel_combo.activated.connect(self.verSelected)        
+        self.prog_sel_combo.activated[str].connect(self.progSelected)
+        self.ver_sel_combo.activated[str].connect(self.verSelected) 
+        self.add_btn.clicked.connect(lambda:    self.add_click())
+        self.save_btn.clicked.connect(lambda:   self.save_click())
 
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('QtGui.QComboBox')
+        self.setGeometry(300, 300, 360, 350)
+        self.setFixedSize(360, 350)
+        self.setWindowTitle('SBGrid Version manager - GBSGrid')
         self.show()
+
+    def populate_table(self, config_array):
+        self.table.clear()        
+        self.table.setRowCount(len(config_array))
+        for i in range(len(config_array)):
+            prog = QtGui.QTableWidgetItem(config_array[i][0])
+            ver = QtGui.QTableWidgetItem(config_array[i][1])
+            self.button = QtGui.QPushButton('Delete')
+            self.button.clicked.connect(self.deleteButtonClicked)
+            self.table.setItem(i,0,prog)
+            self.table.setItem(i,1,ver)
+            self.table.setCellWidget(i,2,self.button)
+
+    def deleteButtonClicked(self):
+        button = QtGui.qApp.focusWidget()
+        # or button = self.sender()
+        index = self.table.indexAt(button.pos())
+        if index.isValid():
+            print(index.row(), index.column())
+            del self.config_array[index.row()]
+        self.populate_table(self.config_array)
+        self.save_btn.setEnabled(True)
         
     def progSelected(self, prog_name):
         print prog_name
-        self.prog_btn.setText('Run ' +prog_name)
+        self.add_btn.setEnabled(True)
+        self.statusBar().showMessage(prog_name+' selected, choose a version')
 
         self.ver_sel_combo.clear()
         for ver in self.prog_dict[str(prog_name)]:	
             self.ver_sel_combo.addItem(ver)
 
-    def verSelected(self, ver_num):
-		print ver_num
+    def verSelected(self, ver_name):
+        print ver_name
+        self.statusBar().showMessage(ver_name+' selected, click <b>add</b> to store change')
+
+    def add_click(self):
+        prog = str(self.prog_sel_combo.currentText())
+        ver = str(self.ver_sel_combo.currentText())
+        self.config_array = backend.add_override(self.config_array, prog, ver)
+        print self.config_array
+        self.save_btn.setEnabled(True)
+        self.populate_table(self.config_array)
+        self.statusBar().showMessage(str(self.prog_sel_combo.currentText())+', version '+str(self.ver_sel_combo.currentText())+' added to change list')
+        self.lbl4.setText('Unsaved Overrides')
+        self.lbl4.adjustSize()
+    
+    def save_click(self):
+        self.lbl4.setText('Saved Overrides')
+        backend.write_config(self.config_header, self.config_array)
+        self.statusBar().showMessage("Changes saved to ~/.sbgrid.conf")
+
 
 	
                 
