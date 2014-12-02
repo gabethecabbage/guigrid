efd2a643074ed4bb72854731a551b64d4fc8289f
Gabriel Schrecker <gabriel.schrecker@postgrad.manchester.ac.uk>
2014-12-02 13:57:09 +0000

    Moved start sh script sym link from /bin to /usr/bin for better RHEL clone support.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
+
 INSTALL_DIR=/opt
 mkdir $INSTALL_DIR/gbsgrid
 cp gbsgrid*	$INSTALL_DIR/gbsgrid/
-
 
 mkdir $INSTALL_DIR/gbsgrid/icons
 cp icons/*	$INSTALL_DIR/gbsgrid/icons/
@@ -12,6 +12,7 @@ ln -s $INSTALL_DIR/gbsgrid/gbsgrid.desktop /usr/share/applications/gbsgrid.deskt
 echo '#! /bin/bash' >> $INSTALL_DIR/gbsgrid/gbsgrid.sh
 echo cd $INSTALL_DIR/gbsgrid >> $INSTALL_DIR/gbsgrid/gbsgrid.sh
 echo python gbsgrid_front.py >> $INSTALL_DIR/gbsgrid/gbsgrid.sh
-ln -s $INSTALL_DIR/gbsgrid/gbsgrid.sh /bin/gbsgrid
+ln -s $INSTALL_DIR/gbsgrid/gbsgrid.sh /usr/bin/gbsgrid
+chmod +x /usr/bin/gbsgrid
 
-
+echo Install finished to $INSTALL_DIR/gbsgrid
